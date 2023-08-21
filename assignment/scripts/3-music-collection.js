e02808334b7fdb2f9c29dd19db147b3b7861e48d
Said Said <ssaid20@icloud.com>
2023-08-20 20:29:33 -0500

    Created the addToCollection function that adds album objects with specified properties (title, artist, yearPublished) to the collection array. The function returns the added album object. Test cases were added to validate the function's behavior by adding multiple albums and logging the results.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,26 @@
 console.log('***** Music Collection *****')
+// Create an empty array to store the collection
+let collection = [];
+// Define the addToCollection function
+function addToCollection(title, artist, yearPublished) {
+  // Create a new album object
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+  // Add the new album object to the collection array
+  collection.push(album);
+  // Return the newly created album object
+  return album;
+}
+// Test the addToCollection function
+console.log(addToCollection('Album 1', 'Artist A', 2020));
+console.log(addToCollection('Album 2', 'Artist B', 2018));
+console.log(addToCollection('Album 3', 'Artist A', 2022));
+console.log(addToCollection('Album 4', 'Artist C', 2015));
+console.log(addToCollection('Album 5', 'Artist B', 2017));
+console.log(addToCollection('Album 6', 'Artist D', 2019));
+
+// Display the collection array
+console.log(collection);
